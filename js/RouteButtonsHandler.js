831b1bef86081e586c7621735d4e0aa97d7ff68d
Victor <vpanya97@gmail.com>
2021-08-30 09:18:05 +0300

    good BEM, less code

--- js/RouteButtonsHandler.js
+++ js/RouteButtonsHandler.js
@@ -1,9 +1,9 @@
-let myActivityBtn = document.querySelector('.myActivity__btn');
+let myActivityBtn = document.querySelector('.my-activity__button');
 myActivityBtn.addEventListener('click', function() {
     window.parent.location = 'index.html';
 });
 
-let addActivityBtn = document.querySelector('.addActivity__btn');
+let addActivityBtn = document.querySelector('.add-activity__button');
 addActivityBtn.addEventListener('click', function() {
     window.parent.location = 'add-activity.html';
 });
