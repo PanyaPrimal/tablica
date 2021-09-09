82806949b713371a32147966a20c6db3eaa864b4
Victor <vpanya97@gmail.com>
2021-09-09 10:14:35 +0300

    uselees code add :c

--- js/formPageEvents.js
+++ js/formPageEvents.js
@@ -2,15 +2,14 @@
 function addFormEventListeners(id) {
     let field = document.getElementById(id);
     field.onfocus = function () {
-        if (this.value == placeholder) {
+        if (this.value == this.placeholder) {
             this.value = '';
         }
     };
     field.onblur = function () {
         if (this.value == '') {
-            this.value = placeholder;
-        } 
+            this.value = this.placeholder;
+        }
     };
 }
 addFormEventListeners('form-section__textarea');
-
